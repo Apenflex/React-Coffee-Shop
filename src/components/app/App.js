f116f5bbd9c72dd9419664ca886e0c130e816b8c
Sviatoslav <blackdbf@gmail.com>
2023-01-05 16:33:26 +0200

    start project

---
+++ src/components/app/App.js
@@ -0,0 +1,14 @@
+
+
+
+function App() {
+  return (
+    <div className="app">
+      <header className="App-header">
+       <h1>Hello Coffee</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
